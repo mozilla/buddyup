56c1b4ad24795b1180b156f0a1783347d3b1ae2a
sole <sole@mozilla.com>
2014-06-10 15:39:28 +0100

    Add comments describing the usage of DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// DOMContentLoaded is fired once the document has been loaded and parsed,
+// but without waiting for other external resources to load (css/images/etc)
+// That makes the app more responsive and perceived as faster.
+// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
 window.addEventListener('DOMContentLoaded', function() {
 
   // We'll ask the browser to use strict code to help us catch errors earlier.
