085041144876bded6c244811e7d34eb359145375
ossreleasefeed <sneethling@mozilla.com>
2014-10-17 14:59:50 -0400

    Removals and changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,6 @@ module.exports = function(grunt) {
 	'use strict';
 
 	grunt.initConfig({
-		less: {
-            dev: {
-                files: {
-                    'css/sandstone.css' : 'less/sandstone/sandstone-resp.less',
-					'css/buddyup.css' : 'less/buddyup.less'
-                }
-            },
-            prod: {
-                options: {
-                    cleancss: true,
-					report: 'min'
-                },
-                files: {
-                    'css/sandstone.css' : 'less/sandstone/sandstone-resp.less',
-					'css/buddyup.css' : 'less/buddyup.less'
-                }
-            }
-        }
+
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-less');
-
-	grunt.registerTask('default', ['less:dev']);
-	grunt.registerTask('dist', ['less:prod']);
 };
