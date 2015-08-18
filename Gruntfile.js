607fc1a8808b0ff0c9df47707ecd6324350ede4f
Mike Cooper <mythmon@gmail.com>
2015-08-18 14:16:35 -0700

    Improve Grunt workflow and documentation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,41 +2,6 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        jshint: {
-            files: ['grunt.js'],
-            options: {
-                bitwise: true,
-                camelcase: true,
-                curly: true,
-                eqeqeq: true,
-                forin: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                quotmark: "single",
-                regexp: true,
-                undef: true,
-                unused: true,
-                trailing: true,
-                browser: true,
-                jquery: true
-            }
-        },
-        csslint: {
-            base_theme: {
-                src: "css/*.css",
-                rules: {
-                    "empty-rules": 2,
-                    "fallback-colors": 2,
-                    "font-sizes": 2,
-                    "important": 2,
-                    "outline-none": 2,
-                    "vendor-prefix": 2,
-                    "zero-units": 2
-                }
-            }
-        },
         nunjucks: {
             precompile: {
                 baseDir: 'app/views/',
@@ -74,10 +39,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.loadNpmTasks('grunt-contrib-jshint')
-    grunt.loadNpmTasks('grunt-css');
-
-    grunt.registerTask('default', ['nunjucks', 'connect', 'watch']);
-    grunt.registerTask('lintify', ['jshint', 'csslint']);
-    grunt.registerTask('precompile', ['nunjucks:precompile']);
+    grunt.registerTask('default', ['nunjucks']);
+    grunt.registerTask('dev', ['nunjucks', 'connect', 'watch']);
 };
